f90c9c16dc37637941abffaddd826e999d0e478e
Piyush Singh <piyushsinghpbh@gmail.com>
2025-09-10 18:36:30 +0530

    Code check in for the spring JDBC sample

---
+++ src/test/java/com/learning/SpringJDBCDemo/SpringJdbcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.SpringJDBCDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
